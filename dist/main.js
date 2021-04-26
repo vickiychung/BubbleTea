99fd0e246fea1b22e33937ce72f14a395c1ad2f6
vickiychung <vickiychung@gmail.com>
2021-04-26 13:50:22 -0700

    Setup webpack and entry point index.js

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Webpack is working!")
+});
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+  console.log("Webpack is working!")
 });
