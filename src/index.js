c3184ecb61d516938057b62f5da300d859991bf0
vickiychung <vickiychung@gmail.com>
2021-04-27 11:33:39 -0700

    Move cat with button clicks

--- src/index.js
+++ src/index.js
@@ -4,27 +4,32 @@ document.addEventListener("DOMContentLoaded", () => {
   let canvas = document.getElementById("game-canvas");
 
   const game = new Game(canvas);
-  let dir = 0, pause = false;
-  loop();
+  let dir = 0, pause = true;
+  // loop();
 
   const leftButton = document.getElementById("left-button");
   const rightButton = document.getElementById("right-button");
-  const pauseButton = document.getElementById("pause-button");
+  // const pauseButton = document.getElementById("pause-button");
 
   leftButton.addEventListener("mousedown", e => {
     dir = -1;
+    pause = !pause;
+    loop();
   });
 
   rightButton.addEventListener("mousedown", e => {
     dir = 1;
-  })
-
-  pauseButton.addEventListener("mousedown", e => {
     pause = !pause;
     loop();
   })
 
+  // pauseButton.addEventListener("mousedown", e => {
+  //   pause = !pause;
+  //   loop();
+  // })
+
   function loop() {
+    console.log(pause);
     if (pause) {
       return cancelAnimationFrame(loop);
     }
