7ed4ff96198ebf2df0fd6894ace97dc07c068627
vickiychung <vickiychung@gmail.com>
2021-04-27 11:51:45 -0700

    Remove game view

--- src/classes/game.js
+++ src/classes/game.js
@@ -7,12 +7,6 @@ class Game {
     this.ctx = canvas.getContext("2d");
     this.dimensions = { width: canvas.width, height: canvas.height };
 
-    // this.update = () => {
-    //   Game.clear();
-    //   this.restart();
-    // }
-
-    // this.registerEvents();
     // this.restart();
     this.play(0);
   }
@@ -30,38 +24,6 @@ class Game {
   //   this.sofa = new Sofa(this.dimensions);
   //   this.table = new Table(this.dimensions);
   //   this.animate(dir);
-  // }
-
-  // registerEvents() {
-  //   this.boundLeftClickHandler = this.leftClick.bind(this);
-  //   this.boundRightClickHandler = this.rightClick.bind(this);
-
-  //   const leftButton = document.getElementById("left-button");
-  //   const rightButton = document.getElementById("right-button")
-  //   leftButton.addEventListener("mousedown", this.boundLeftClickHandler);
-  //   rightButton.addEventListener("mousedown", this.boundRightClickHandler);
-  // }
-
-  // leftClick(e) {
-  //   if (!this.playing) {
-  //     this.play();
-  //   }
-  //   this.dir = -1;
-
-  //   console.log("left")
-
-  //   // this.update();
-  // }
-
-  // rightClick(e) {
-  //   if (!this.playing) {
-  //     this.play();
-  //   }
-  //   this.dir = 1;
-
-  //   console.log("right")
-
-  //   // this.update();
   // }
   
   animate(dir) {
