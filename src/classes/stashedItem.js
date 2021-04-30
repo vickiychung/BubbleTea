ab1b615960ad5c8379ac0e13bbd5f78c219f6794
vickiychung <vickiychung@gmail.com>
2021-04-30 10:57:01 -0700

    Add stashedItem class

---
+++ src/classes/stashedItem.js
@@ -0,0 +1,24 @@
+const CONSTANTS = {
+  ITEM_WIDTH: 60,
+  ITEM_HEIGHT: 60
+};
+
+const itemImg = new Image();
+itemImg.src = './dist/assets/images/item.png';
+
+class stashedItem {
+  constructor(dimensions) {
+    this.dimensions = dimensions;
+    this.x = (this.dimensions.width - CONSTANTS.SOFA_WIDTH - 20) * Math.random();
+    this.y = this.dimensions.height - 120;
+    this.img = itemImg;
+  }
+
+  drawItem(ctx) {
+    ctx.fillStyle = "transparent";
+    ctx.fillRect(this.x, this.y, CONSTANTS.ITEM_WIDTH, CONSTANTS.ITEM_HEIGHT);
+    ctx.drawImage(this.img, this.x, this.y, CONSTANTS.ITEM_WIDTH, CONSTANTS.ITEM_HEIGHT);
+  }
+}
+
+module.exports = stashedItem;
